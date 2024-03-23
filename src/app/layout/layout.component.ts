92b491a1eb5c5c97776666e71bd905687c605b63
Mahtab Alam <mehtab.makent@gmail.com>
2024-03-23 15:50:07 +0530

    change meta title and discrition on homepage and fissure page

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,10 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal, NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 import { AppointmentService } from '../services/appointment/appointment.service';
@@ -12,12 +17,11 @@ import { DomSanitizer, Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
-
   showCar = false;
-  specialities:any[] = [];
+  specialities: any[] = [];
   surgeries: any[] = [];
   allSurgeries: any[] = [];
   visibleCount = 0;
@@ -31,51 +35,61 @@ https://www.youtube.com/shorts/tjRTxKdIqic
 https://www.youtube.com/shorts/iQ3bV0QcWVA
 https://www.youtube.com/shorts/ibNe6LcbeF4 */
   youtubeVideos: any[] = [
-    { url: "xZMbj4GXAoI", desciption: 'Mr Anil is back to his healthy & amazing life after his succesful surgery with @surgify' },
-    { url: "tjRTxKdIqic", description: 'Mr. sagar borigam experiencing a kidney stone surgery with surgify' },
-    { url: "iQ3bV0QcWVA", description: 'Mallesham Borigam at Surgify' },
-    { url: "ibNe6LcbeF4", description: 'Varsha rajgole with a successful hernia surgery at SURGIFY' }
+    {
+      url: 'xZMbj4GXAoI',
+      desciption:
+        'Mr Anil is back to his healthy & amazing life after his succesful surgery with @surgify',
+    },
+    {
+      url: 'tjRTxKdIqic',
+      description:
+        'Mr. sagar borigam experiencing a kidney stone surgery with surgify',
+    },
+    { url: 'iQ3bV0QcWVA', description: 'Mallesham Borigam at Surgify' },
+    {
+      url: 'ibNe6LcbeF4',
+      description: 'Varsha rajgole with a successful hernia surgery at SURGIFY',
+    },
   ];
   testimonialData = [
     {
       name: 'Mr. Shantaram, 48Yrs',
       location: 'Hyderabad',
-      text: 'I was having a lot of trouble while sitting or doing my daily activities. I got my hip replacement surgery done from Surgify. The doctors were very helpful, overall a very good service by the staff, i didn\'t have to worry about food or my transportation.'
+      text: "I was having a lot of trouble while sitting or doing my daily activities. I got my hip replacement surgery done from Surgify. The doctors were very helpful, overall a very good service by the staff, i didn't have to worry about food or my transportation.",
     },
     {
       name: 'Mr. Prakash, 48Yrs',
       location: 'Mumbai',
-      text: "I was suffering with Piles and until I got my surgery done life was very stressful living with Piles. But after my surgery from Surgify I am very relaxed now there is no pain or discomfort, I got everything done quickly by Surgify team. I will surely recommend this service."
+      text: 'I was suffering with Piles and until I got my surgery done life was very stressful living with Piles. But after my surgery from Surgify I am very relaxed now there is no pain or discomfort, I got everything done quickly by Surgify team. I will surely recommend this service.',
     },
     {
       name: 'Mrs. Varsha, 43Yrs',
       location: 'Hyderabad',
-      text: "Was suffering with Hernia for the past 8 months, then I contacted Surgify and got my appointment and surgery. I got the right treatment at the right time, with all the facilities like food and pick up drop from my home, even the room was very good and comfortable."
+      text: 'Was suffering with Hernia for the past 8 months, then I contacted Surgify and got my appointment and surgery. I got the right treatment at the right time, with all the facilities like food and pick up drop from my home, even the room was very good and comfortable.',
     },
     {
       name: 'Mrs. Jayshree, 56yrs',
       location: 'Hyderabad',
-      text: "We got a cataract surgery done for my mother who is 75, we were worried about the mediclaim but with Surgify we got all the benefits, till our Surgery they had their representative with us so we got all the help pickup and drop, food was also tasty and comfortable rooms to stay for the patient relative.A very good experience with Surgify."
+      text: 'We got a cataract surgery done for my mother who is 75, we were worried about the mediclaim but with Surgify we got all the benefits, till our Surgery they had their representative with us so we got all the help pickup and drop, food was also tasty and comfortable rooms to stay for the patient relative.A very good experience with Surgify.',
     },
     {
       name: 'Mrs. Anna Iturbe',
       location: 'Hyderabad',
-      text: 'Very pleased with the company. Everything was done in a timely manner. Your service and assistance with the process were very, very good. I would refer Surgify to my friends and family.'
+      text: 'Very pleased with the company. Everything was done in a timely manner. Your service and assistance with the process were very, very good. I would refer Surgify to my friends and family.',
     },
     {
       name: 'Mrs. Lara Atkinson',
       location: 'Hyderabad',
-      text: 'Very pleased with the company. Everything was done in a timely manner. Your service and assistance with the process were very, very good. I would refer Surgify to my friends and family.'
-    }
+      text: 'Very pleased with the company. Everything was done in a timely manner. Your service and assistance with the process were very, very good. I would refer Surgify to my friends and family.',
+    },
   ];
-  randomSurgeries: any[] = [
-
-  ];
+  randomSurgeries: any[] = [];
 
   surgeriesPerfom: any = [];
   surgeriesPerfomLoading = true;
 
-  constructor(private specializationService: SpecializationService,
+  constructor(
+    private specializationService: SpecializationService,
     private surgeriesService: SurgeriesService,
     private modalService: NgbModal,
     private appointmentService: AppointmentService,
@@ -84,15 +98,25 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
     private hospitalService: HospitalService,
     public sanitizer: DomSanitizer,
     private titleService: Title,
-    private meta: Meta) {
+    private meta: Meta
+  ) {
     this.videoReqForm = this.fb.group({
       patientName: new FormControl('', Validators.required),
-      mobileNo: new FormControl('', [Validators.required, Validators.pattern("^[0-9]{10}$")])
+      mobileNo: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]{10}$'),
+      ]),
     });
     this.onResize();
-    this.titleService.setTitle('Sugify is one-stop solution for surgeries');
+    this.titleService.setTitle('Surgify: Best Piles & Fistula Clinic in India');
     this.meta.updateTag({ content: '' }, "name='keywords'");
-    this.meta.updateTag({ content: '' }, "name='description'")
+    this.meta.updateTag(
+      {
+        content:
+          'Your well-being is our priority. Book an appointment at Surgify Piles & Fistula Clinic for compassionate care and support on your journey to recovery.',
+      },
+      "name='description'"
+    );
 
     // let videos = Object.assign([], this.youtubeVideos);
     // let sanity: any[] = [];
@@ -100,10 +124,8 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
     //   sanity.push(this.sanitizer.bypassSecurityTrustUrl(videos[ind]));
     // });
 
-
     // this.youtubeVideos = [];
     // this.youtubeVideos = sanity;
-
   }
 
   ngOnInit(): void {
@@ -117,57 +139,67 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
     this.specializationService.getAllSpecializations().subscribe({
       next: (specialists) => {
         if (specialists && specialists.length > 0) {
-
           specialists.forEach((sp: any, spInd: number) => {
-            this.surgeriesService.getAllSurgeriesBySpecialization(sp.id).subscribe((surgeries) => {
-              this.surgeries.push({ specialization: sp.name, surgeries: surgeries });
-              this.allSurgeries = this.allSurgeries.concat(surgeries);
-              this.surgeriesPerfomLoading = false;
-              if (spInd == specialists.length - 1) {
-                // console.log(spInd, specialists);
-                this.allSurgeries.forEach((sur, ind) => {
-                  let findIt = this.randomSurgeries.find((s) => s.name.toLowerCase() == sur.name.toLowerCase());
-                  if (!findIt) {
-                    this.randomSurgeries.push(sur);
-                  } else {
-
-                  }
-                });
-
-
-                if (this.allSurgeries.length == this.randomSurgeries.length) {
+            this.surgeriesService
+              .getAllSurgeriesBySpecialization(sp.id)
+              .subscribe(
+                (surgeries) => {
+                  this.surgeries.push({
+                    specialization: sp.name,
+                    surgeries: surgeries,
+                  });
+                  this.allSurgeries = this.allSurgeries.concat(surgeries);
                   this.surgeriesPerfomLoading = false;
-                  this.setRandom();
+                  if (spInd == specialists.length - 1) {
+                    // console.log(spInd, specialists);
+                    this.allSurgeries.forEach((sur, ind) => {
+                      let findIt = this.randomSurgeries.find(
+                        (s) => s.name.toLowerCase() == sur.name.toLowerCase()
+                      );
+                      if (!findIt) {
+                        this.randomSurgeries.push(sur);
+                      } else {
+                      }
+                    });
+
+                    if (
+                      this.allSurgeries.length == this.randomSurgeries.length
+                    ) {
+                      this.surgeriesPerfomLoading = false;
+                      this.setRandom();
+                    }
+                  }
+                },
+                (err) => {
+                  // console.log(err);
                 }
-
-              }
-
-            }, (err) => {
-              // console.log(err);
-            });
+              );
           });
         }
         this.specialities = specialists;
-
-      }, error: (error) => {
+      },
+      error: (error) => {
         // console.log(error);
-      }
-    })
+      },
+    });
   }
 
-  openBookDialog(specialization?:any) {
-    const mod = this.modalService.open(BookingComponent, { windowClass: 'appointment-modal ', modalDialogClass: 'modal-dialog-centered' });
-    if(specialization){
-      let found = this.allSurgeries.find((sur)=> sur.name.toLowerCase()==specialization.toLowerCase());
+  openBookDialog(specialization?: any) {
+    const mod = this.modalService.open(BookingComponent, {
+      windowClass: 'appointment-modal ',
+      modalDialogClass: 'modal-dialog-centered',
+    });
+    if (specialization) {
+      let found = this.allSurgeries.find(
+        (sur) => sur.name.toLowerCase() == specialization.toLowerCase()
+      );
       mod.componentInstance.disease = found;
     }
   }
 
-
   randomInteger(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
 
   setRandom() {
     let randomNums: number[] = [];
@@ -182,9 +214,20 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
 
     this.surgeriesPerfom = [];
     let tempAssign: any = [];
-    let list = ['Piles', 'Fissure', 'Fistula', 'Appendicitis Treatment ', 'Hernia Treatment', 'Cataract', 'Phimosis', 'Gallstone Treatment'];
+    let list = [
+      'Piles',
+      'Fissure',
+      'Fistula',
+      'Appendicitis Treatment ',
+      'Hernia Treatment',
+      'Cataract',
+      'Phimosis',
+      'Gallstone Treatment',
+    ];
     list.forEach((sur) => {
-      let index = this.randomSurgeries.findIndex((s) => s.name.toLowerCase() == sur.toLowerCase());
+      let index = this.randomSurgeries.findIndex(
+        (s) => s.name.toLowerCase() == sur.toLowerCase()
+      );
       tempAssign.push(this.randomSurgeries[index]);
       this.randomSurgeries.splice(index, 1);
     });
@@ -195,7 +238,6 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
     this.surgeriesPerfom = tempAssign;
     // console.log(this.surgeriesPerfom);
   }
-
 
   @HostListener('window:resize', ['$event'])
   onResize(event?: any) {
@@ -214,7 +256,6 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
     }
   }
 
-
   submitVideReq() {
     if (this.videoReqForm.invalid) return;
     let formData = this.videoReqForm.value;
@@ -227,10 +268,9 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
         if (result) {
           this.videoReqForm.reset();
         }
-      }
-    })
+      },
+    });
   }
-
 
   togglePopover(popover: NgbPopover, index: number) {
     if (popover.isOpen()) {
@@ -241,13 +281,16 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
   }
 
   goToDetail(surgery: any) {
-    let speciality = this.specialities.find((sp)=> sp?.id===surgery.specializationId);
+    let speciality = this.specialities.find(
+      (sp) => sp?.id === surgery.specializationId
+    );
 
-    let findIt = this.randomSurgeries.find((s) => s.name.toLowerCase() == surgery.name.toLowerCase());
-    if(speciality && findIt){
+    let findIt = this.randomSurgeries.find(
+      (s) => s.name.toLowerCase() == surgery.name.toLowerCase()
+    );
+    if (speciality && findIt) {
       this.router.navigateByUrl(speciality?.slug + '/' + surgery?.slug);
     }
-    
   }
 
   hasError(name: string, errorType: string) {
@@ -256,24 +299,26 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
   }
 
   validateInput(event: any) {
-    if (typeof event.target.value == "number") {
-
+    if (typeof event.target.value == 'number') {
     } else {
       event.preventDefault();
     }
   }
 
   getAllHospitals() {
-    this.hospitalService.getAllHospitals(6).subscribe((list) => {
-      if (list && list.length > 0) {
-        this.allHospitals = list;
-        this.setRandomHospitals();
-      } else {
+    this.hospitalService.getAllHospitals(6).subscribe(
+      (list) => {
+        if (list && list.length > 0) {
+          this.allHospitals = list;
+          this.setRandomHospitals();
+        } else {
+          this.allHospitals = [];
+        }
+      },
+      (err) => {
         this.allHospitals = [];
       }
-    }, (err) => {
-      this.allHospitals = [];
-    });
+    );
   }
 
   setRandomHospitals() {
@@ -298,14 +343,19 @@ https://www.youtube.com/shorts/ibNe6LcbeF4 */
 
   openVideo(videoId: any, content: any) {
     this.currentVideo = videoId;
-    this.modalService.open(content, { modalDialogClass: 'modal-dialog-centered', windowClass: 'loginPup videoPup' })
+    this.modalService
+      .open(content, {
+        modalDialogClass: 'modal-dialog-centered',
+        windowClass: 'loginPup videoPup',
+      })
       .shown.subscribe({
         next: (s) => {
-          let ele = document.getElementById('videoP')?.querySelector('iframe')?.classList.add('ngVideo');
-
+          let ele = document
+            .getElementById('videoP')
+            ?.querySelector('iframe')
+            ?.classList.add('ngVideo');
         },
-        error: (err) => {
-        }
+        error: (err) => {},
       });
   }
 }
